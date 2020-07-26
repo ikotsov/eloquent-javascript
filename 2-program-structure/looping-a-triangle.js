425ded0127449dfe57e166b51461eb70dc9022ca
ikotsov <i.kotsovilis@gmail.com>
2020-07-26 17:59:59 +0300

    add second solution and remove carriage return utility

--- 2-program-structure/looping-a-triangle.js
+++ 2-program-structure/looping-a-triangle.js
@@ -1,10 +1,23 @@
+// Solution 1
 function getTriangle(size) {
-    let output = '';
     for (let i = 1; i < size + 1; i++) {
-        output += getNumOfSharps(i) + getCarriageReturn();
+        let output = getNumOfSharps(i);
         console.log(output);
     }
 }
+
+getTriangle(7);
+
+// Solution 2
+function getTriangle2(size) {
+    let sharp = getSharp();
+    while(sharp.length <= size) {
+        console.log(sharp);
+        sharp += getSharp();
+    }
+}
+
+getTriangle2(7);
 
 // Utilities
 function getNumOfSharps(total) {
@@ -18,9 +31,3 @@ function getNumOfSharps(total) {
 function getSharp() {
     return '#';
 }
-
-function getCarriageReturn() {
-    return '\r';
-}
-
-getTriangle(7);
