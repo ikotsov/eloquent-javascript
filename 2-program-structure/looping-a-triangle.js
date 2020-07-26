69371e3b774b5e57990c193c61359e27b49dd0c3
ikotsov <i.kotsovilis@gmail.com>
2020-07-26 15:26:10 +0300

    change structure and rename func

--- 2-program-structure/looping-a-triangle.js
+++ 2-program-structure/looping-a-triangle.js
@@ -1,19 +1,15 @@
 function getTriangle(size) {
     let output = '';
     for (let i = 1; i < size + 1; i++) {
-        output += getSharpBy(i) + getCarriageReturn();
+        output += getNumOfSharps(i) + getCarriageReturn();
         console.log(output);
     }
 }
 
 // Utilities
-function getCarriageReturn() {
-    return '\r';
-}
-
-function getSharpBy(num) {
+function getNumOfSharps(total) {
     let output = '';
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < total; i++) {
         output += getSharp();
     }
     return output;
@@ -21,6 +17,10 @@ function getSharpBy(num) {
 
 function getSharp() {
     return '#';
+}
+
+function getCarriageReturn() {
+    return '\r';
 }
 
 getTriangle(7);
